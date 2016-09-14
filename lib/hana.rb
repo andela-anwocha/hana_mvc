8fcb99eaae1abb5ee87fe29a7673bfa9e370a512
andela-anwocha <adimoranma.felix@andela.com>
2016-09-15 00:27:52 +0100

    Initial Commit

---
+++ lib/hana.rb
@@ -0,0 +1,36 @@
+require 'pry'
+require 'json'
+require 'hana/version'
+require 'hana/utils/string'
+require 'hana/utils/hash'
+require 'hana/dependencies'
+require 'hana/controller/controller'
+require 'hana/routing/router'
+require 'hana/routing/route'
+require 'hana/routing/mapper'
+require 'hana/orm/base_model'
+
+
+module Hana
+  class Application
+    @@routes = Hana::Routing::Router.new
+
+    def self.routes
+      @@routes
+    end
+
+    def call(env)
+      @request = Rack::Request.new(env)
+      route = mapper.map_to_route(@request)
+      if route
+        response = route.dispatch
+        return response
+      end
+      [404, {}, ['Route not found']]
+    end
+
+    def mapper
+      @mapper ||= Routing::Mapper.new(@@routes.endpoints)
+    end
+  end
+end
