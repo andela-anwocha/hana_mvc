3a6052bf4ef95c2e11fac8a19c80ee19268e5feb
andela-anwocha <adimoranma.felix@andela.com>
2016-09-15 01:33:58 +0100

    test: add tests for queries

---
+++ spec/orm/query_helpers_spec.rb
@@ -0,0 +1,36 @@
+require "spec_helper"
+
+describe "QueryHelpers" do
+  let(:db){ Hana::Database }
+  subject{ Hana::BaseModel }
+
+  describe ".to_table" do
+    it "assigns a table name to an instance variable" do
+      subject.to_table :users
+      table_name = subject.instance_variable_get(:@table_name)
+
+      expect(table_name).to eq(:users)
+    end
+  end
+
+  describe ".property" do
+    before { subject.property :name, type: :integer }
+
+    it "populates the properties instance variable" do
+      property = subject.instance_variable_get(:@properties)
+
+      expect(property.keys).to include(:name)
+    end
+
+    it "makes the column names attribute accessible" do
+      expect(subject.new).to respond_to(:name)
+    end
+  end
+
+  describe ".create_table" do
+    it "executes sql query to create database table" do
+      expect(db).to receive(:execute)
+      subject.create_table
+    end
+  end
+end
