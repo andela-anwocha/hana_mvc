3a80d263a0b0e619eeb98e7c98fb090eaa8477c9
andela-anwocha <adimoranma.felix@andela.com>
2016-09-15 01:58:54 +0100

    add generator templates

---
+++ generator_templates/app/models/model.rb
@@ -0,0 +1,15 @@
+class Model < Hana::BaseModel
+  # Add model properties below
+
+  # Examples
+  # to_table :todos
+  # property :title, type: :text, nullable: false
+  # property :age, type: :integer
+  # property :description, type: :text
+  # create_table
+
+  # validates :title, presence: true
+  # validates :age, numericality: true
+  # validates :description, length: { is: 9 }
+  # validates :description, length: { minimum: 3, maximum: 9 }
+end
